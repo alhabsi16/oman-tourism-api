e79483a93d3210e53cbbf877fb5ab1c174badb67
alhabsi16 <mxmx8283@gmail.com>
2023-12-23 23:44:14 +0400

    Added an empty springboot project

---
+++ src/test/java/com/omantourism/datamanager/DataManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omantourism.datamanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
